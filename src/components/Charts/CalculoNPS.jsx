6933dfcbb95dca6e4b9e5c46db07912de2cc32bc
Ignacio Velazquez <ignaciov92@gmail.com>
2023-07-31 23:41:59 -0300

    se realiza calculo de pilares

--- src/components/Charts/CalculoNPS.jsx
+++ src/components/Charts/CalculoNPS.jsx
@@ -13,49 +13,110 @@ const CalculoNPS = ({ data }) => {
   const detractors = data.filter(item => item.NPS_GROUP === 'Detractor').length;
   const neutrals = data.filter(item => item.NPS_GROUP === 'Pasivo').length;
 
+   //filtro de Pilares Cordialidad, conocimiento , Claridad
+
+  const calculoPilares = (filtro) =>{    
+    const muySatisfecho = data.filter(item => item[filtro] === 'Muy satisfecho').length;
+    const bastanteSatisfecho = data.filter(item => item[filtro] === 'Bastante satisfecho').length;
+    const niSatNiInsat = data.filter(item => item[filtro] === 'Ni satisfecho ni insatisfecho').length;
+    const pocoSatisfecho = data.filter(item => item[filtro] === 'Poco satisfecho').length;
+    const nadaSatisfecho = data.filter(item => item[filtro] === 'Nada satisfecho').length;
+  
+  
+    const metricaPilar = (muySatisfecho + bastanteSatisfecho)/(muySatisfecho+bastanteSatisfecho+niSatNiInsat+pocoSatisfecho+nadaSatisfecho) *100
+  
+
+    return metricaPilar.toFixed(2)
+  }
+
+  const calculoResolución = () =>{    
+    const resuelto = data.filter(item => item.Resolucion === 'Si').length;
+    const noResuelto = data.filter(item => item.Resolucion === 'No').length;
+
+  
+    const metricaPilar = resuelto /(resuelto+noResuelto) *100
+  
+
+    return metricaPilar.toFixed(2)
+  }
+
+
+  const Cordialidad = calculoPilares("Cordialidad");
+  const Claridad = calculoPilares("Claridad");
+  const Conocimiento = calculoPilares("Conocimiento");
+  const Resolucion = calculoResolución();
+
+
+
   // Calcular el NPS
   const totalResponses = promoters + detractors + neutrals;
   const nps = calculateNps(promoters, detractors, totalResponses);
 
-// //empaquetar
+ //Calculo de Cordialidad
 
-// const data = [
-//   { Clientes: 'Promotores', count: promoters },
-//   { Clientes: 'Detractores', count: detractors },
-//   { Clientes: 'Neutros', count: neutrals },
-// ];
+ //Calculo de conocimiento
+
+
+ //Calculo de claridad
+//  calculo de resolución
 
   return (
 
     <>
     <h1 className='text-center'>Resultados:</h1>
-    <table className="table table-hover my-3 responsive container">
-      <tbody>
-      <tr className="table-success">
-        <th scope="row">Promotores</th>
-        <td>{promoters}</td>
-      </tr>
+      <div className='d-lg-flex justify-content-evenly '>
+        <table className="table table-hover my-3 responsive container table-bordered ">
+          <tbody>
+          <tr className="table-success">
+            <th scope="row">Promotores</th>
+            <td>{promoters}</td>
+          </tr>
 
-      <tr className="table-danger">
-        <th scope="row">Detractores</th>
-        <td>{detractors}</td>
-      </tr>
+          <tr className="table-danger">
+            <th scope="row">Detractores</th>
+            <td>{detractors}</td>
+          </tr>
 
-      <tr className="table-warning">
-        <th scope="row">Neutros</th>
-        <td>{neutrals}</td>
-      </tr>
+          <tr className="table-warning">
+            <th scope="row">Neutros</th>
+            <td>{neutrals}</td>
+          </tr>
 
-      <tr className="table-light">
-        <th scope="row">Total de respuestas:</th>
-        <td>{totalResponses}</td>
-      </tr>
-      <tr className="table-info">
-        <th scope="row">NPS</th>
-        <td>{nps}%</td>
-      </tr>
-      </tbody>
-    </table>
+          <tr className="table-light">
+            <th scope="row">Total de respuestas:</th>
+            <td>{totalResponses}</td>
+          </tr>
+          <tr className="table-info">
+            <th scope="row">NPS</th>
+            <td>{nps}%</td>
+          </tr>
+          </tbody>
+        </table>
+
+        <table className="table table-hover my-3 responsive container table-bordered table-striped table-success">
+          <tbody>
+          <tr >
+            <th scope="row">Cordialidad</th>
+            <td>{Cordialidad}%</td>
+          </tr>
+
+          <tr >
+            <th scope="row">Claridad</th>
+            <td>{Claridad}%</td>
+          </tr>
+
+          <tr >
+            <th scope="row">Conocimiento</th>
+            <td>{Conocimiento}%</td>
+          </tr>
+
+          <tr >
+            <th scope="row">Resolucion</th>
+            <td>{Resolucion}%</td>
+          </tr>
+          </tbody>
+        </table>
+      </div>
     </>
 
   );
