b09f2c8be6058b6ba74cb4030e21082caa1a7305
Ignacio Velazquez <ignaciov92@gmail.com>
2023-08-08 23:51:56 -0300

    se agrega chart

--- src/components/Charts/BarChart.jsx
+++ src/components/Charts/BarChart.jsx
@@ -1,39 +1,18 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { Bar } from 'react-chartjs-2';
 
-const NPSChart = ({ npsData }) => {
-  // Obtener los datos y las etiquetas para el gráfico
-  const scores = npsData.map((data) => data.score);
-  const days = npsData.map((data) => data.day);
 
-  // Configuración del gráfico
-  const chartData = {
-    labels: days,
-    datasets: [
-      {
-        label: 'NPS por Día',
-        data: scores,
-        backgroundColor: 'rgba(54, 162, 235, 0.6)',
-      },
-    ],
-  };
+const BarChart = ({ npsData }) => {
 
-  const chartOptions = {
-    scales: {
-      y: {
-        beginAtZero: true,
-        stepSize: 1,
-        suggestedMax: 10,
-      },
-    },
-  };
+
+  console.log(npsData);
+ 
 
   return (
     <div>
       <h2>Net Promoter Score por Día</h2>
-      <Bar data={chartData} options={chartOptions} />
     </div>
   );
 };
 
-export default NPSChart;
+export default BarChart;
--- src/components/Home/StartekNPS.jsx
+++ src/components/Home/StartekNPS.jsx
@@ -1,30 +1,33 @@
-import React, { useState,useEffect } from 'react';
-import DatePicker from 'react-datepicker';
-import 'react-datepicker/dist/react-datepicker.css';
-import { ApiClient } from '../../api/services';
-import CalculoNPS from '../Charts/CalculoNPS';
-import Loader from '../Loader/Loader';
-
+import React, { useState, useEffect } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import { ApiClient } from "../../api/services";
+import CalculoNPS from "../Charts/CalculoNPS";
+import Loader from "../Loader/Loader";
+import LineChartDay from "../Charts/LineChartDay";
 
 const EncuestasPorMes = () => {
-  const [encuestas, setEncuestas]=useState([])
+  const [encuestas, setEncuestas] = useState([]);
+  const [npsDia, setNpsDia] = useState([]);
   const [startDate, setStartDate] = useState(null);
   const [endDate, setEndDate] = useState(new Date()); // Establecer "hasta" como el día de hoy por defecto
   const [loading, setLoading] = useState(false);
   const apiClient = new ApiClient();
 
-
   useEffect(() => {
     // Configurar la fecha "desde" por defecto al primer día del mes en curso
-    const firstDayOfMonth = new Date(new Date().getFullYear(), new Date().getMonth(), 1);
+    const firstDayOfMonth = new Date(
+      new Date().getFullYear(),
+      new Date().getMonth(),
+      1
+    );
     setStartDate(firstDayOfMonth);
   }, []);
 
-
   const handleDateChange = (date, field) => {
-    if (field === 'from') {
+    if (field === "from") {
       setStartDate(date);
-    } else if (field === 'to') {
+    } else if (field === "to") {
       setEndDate(date);
     }
   };
@@ -32,49 +35,79 @@ const EncuestasPorMes = () => {
   const getEncuestasPorRangoFechas = async () => {
     // Verificar que ambas fechas estén seleccionadas
     if (!startDate || !endDate) return;
-    
+
     setLoading(true);
     // Formatear las fechas para enviarlas al backend en el formato "dd/MM/yyyy"
     const fromDate = formatDate(startDate);
     const toDate = formatDate(endDate);
-  
+
     try {
       const objetoFecha = { desde: fromDate, hasta: toDate };
       const response = await apiClient.getNpsbyDate(objetoFecha);
-      setEncuestas(response.data)
+      setEncuestas(response.data);
     } catch (error) {
       console.log(error);
-    }finally {
+    } finally {
       setLoading(false);
     }
   };
 
   const formatDate = (date) => {
     // Obtener el día, mes y año de la fecha y formatearla como "dd/MM/yyyy"
-    const day = String(date.getDate()).padStart(2, '0');
-    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, "0");
+    const month = String(date.getMonth() + 1).padStart(2, "0");
     const formattedDate = `${day}/${month}/${date.getFullYear()}`;
     return formattedDate;
   };
 
+  const calculateNPSByDay = () => {
+    // Agrupar las encuestas por día
+    const groupedEncuestas = encuestas.reduce((result, encuesta) => {
+      const fecha = new Date(encuesta.Fecha).toISOString().slice(0, 10); // Extraer solo la parte de la fecha (sin la hora)
+      if (!result[fecha]) {
+        result[fecha] = [];
+      }
+      result[fecha].push(encuesta);
+      return result;
+    }, {});
+
+    // Calcular el NPS para cada día y guardar el resultado en un array
+    const npsPorDia = [];
+    for (const fecha in groupedEncuestas) {
+      const promotores = groupedEncuestas[fecha].filter(
+        (encuesta) => encuesta.NPS_Calification >= 9
+      ).length;
+      const detractores = groupedEncuestas[fecha].filter(
+        (encuesta) => encuesta.NPS_Calification <= 6
+      ).length;
+      const totalRespuestas = groupedEncuestas[fecha].length;
+      const nps = ((promotores - detractores) / totalRespuestas) * 100;
+      npsPorDia.push({ fecha, nps });
+    }
+
+    return npsPorDia;
+  };
+
+  console.log(calculateNPSByDay());
+
   return (
     <div>
-      <div className='d-flex flex-column flex-md-row justify-content-center align-items-md-center'>
-        <div className='d-flex justify-content-center'>
+      <div className="d-flex flex-column flex-md-row justify-content-center align-items-md-center">
+        <div className="d-flex justify-content-center">
           <DatePicker
             selected={startDate}
-            onChange={(date) => handleDateChange(date, 'from')}
+            onChange={(date) => handleDateChange(date, "from")}
             selectsStart
             startDate={startDate}
             endDate={endDate}
             dateFormat="dd/MM/yyyy"
             isClearable
             placeholderText="Desde"
-            className='me-2 mb-2 rounded py-1 form-control  form-control-lg'
+            className="me-2 mb-2 rounded py-1 form-control  form-control-lg"
           />
           <DatePicker
             selected={endDate}
-            onChange={(date) => handleDateChange(date, 'to')}
+            onChange={(date) => handleDateChange(date, "to")}
             selectsEnd
             startDate={startDate}
             endDate={endDate}
@@ -82,18 +115,29 @@ const EncuestasPorMes = () => {
             dateFormat="dd/MM/yyyy"
             isClearable
             placeholderText="Hasta"
-            className='me-2 mb-2 rounded py-1 form-control form-control-lg'
+            className="me-2 mb-2 rounded py-1 form-control form-control-lg"
           />
-
         </div>
-        <div className='d-flex justify-content-center'>
-          <button onClick={getEncuestasPorRangoFechas} className="btn btn-outline-primary btn-lg mb-2">Calcular NPS</button>
+        <div className="d-flex justify-content-center">
+          <button
+            onClick={getEncuestasPorRangoFechas}
+            className="btn btn-outline-primary btn-lg mb-2"
+          >
+            Calcular NPS
+          </button>
         </div>
       </div>
-      {
-        loading && <Loader className="mx-auto"/>
-      }
-      <CalculoNPS data={encuestas} />
+      {loading && <Loader className="mx-auto" />}
+      {encuestas && <CalculoNPS data={encuestas} />}
+
+      {encuestas && (
+        <div className="row d-flex justify-content-center">
+          <LineChartDay
+            className="col-lg-8 col-12 mx-auto"
+            data={calculateNPSByDay()}
+          />
+        </div>
+      )}
     </div>
   );
 };
